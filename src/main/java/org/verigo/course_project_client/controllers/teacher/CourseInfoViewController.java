8f1c111a5764f31a1618a318fd14358bb9e05c26
misxka <mishutkavlesu@gmail.com>
2021-11-28 19:29:57 +0300

    feat: add course-info view

--- src/main/java/org/verigo/course_project_client/controllers/teacher/CourseInfoViewController.java
+++ src/main/java/org/verigo/course_project_client/controllers/teacher/CourseInfoViewController.java
@@ -1,13 +1,32 @@
 package org.verigo.course_project_client.controllers.teacher;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.javafx.Icon;
 import org.verigo.course_project_client.models.Course;
+import org.verigo.course_project_client.models.Task;
 
 public class CourseInfoViewController {
     private Course course;
+
+    @FXML
+    private Label courseInfoLabel;
+
+    @FXML
+    private AnchorPane treeTableViewContainer;
+
+
+    TreeTableView<Task> treeTableView = new TreeTableView<>();
 
 
     public void setCourse(Course course) {
@@ -16,6 +35,63 @@ public class CourseInfoViewController {
 
     @FXML
     public void initialize() {
+        courseInfoLabel.setText("Структура курса " + course.getTitle());
 
+        initTreeTableView();
+        fillTreeTableView();
+    }
+
+    private void initTreeTableView() {
+        treeTableView.setLayoutX(0);
+        treeTableView.setLayoutY(0);
+
+        TreeTableColumn<Task, String> titleColumn = new TreeTableColumn<>("Название");
+        titleColumn.setPrefWidth(150);
+        TreeTableColumn<Task, String> descriptionColumn = new TreeTableColumn<>("Описание");
+        descriptionColumn.setPrefWidth(250);
+        TreeTableColumn<Task, FontIcon> hometaskColumn = new TreeTableColumn<>("Домашнее задание?");
+        hometaskColumn.setPrefWidth(140);
+        hometaskColumn.setStyle("-fx-alignment: CENTER");
+        TreeTableColumn<Task, String> maxPointsColumn = new TreeTableColumn<>("Макс. оценка");
+        maxPointsColumn.setPrefWidth(100);
+        maxPointsColumn.setStyle("-fx-alignment: CENTER");
+
+        titleColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("title"));
+        descriptionColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("description"));
+        hometaskColumn.setCellValueFactory(data -> {
+            String value = data.getValue().getValue().getIsHometaskString();
+            FontIcon trueIcon = new FontIcon("fas-check");
+            trueIcon.setIconColor(Color.GREEN);
+            trueIcon.setIconSize(14);
+
+            FontIcon falseIcon = new FontIcon("fas-times");
+            falseIcon.setIconColor(Color.RED);
+            falseIcon.setIconSize(14);
+
+            return (value.equals("") ? new SimpleObjectProperty<>(new FontIcon()) : (value.equals("true") ? new SimpleObjectProperty<>(trueIcon) : new SimpleObjectProperty<>(falseIcon)));
+        });
+        maxPointsColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("maxPointsString"));
+
+        treeTableView.getColumns().add(titleColumn);
+        treeTableView.getColumns().add(descriptionColumn);
+        treeTableView.getColumns().add(hometaskColumn);
+        treeTableView.getColumns().add(maxPointsColumn);
+
+        treeTableViewContainer.getChildren().add(treeTableView);
+    }
+
+    private void fillTreeTableView() {
+        TreeItem lessons = new TreeItem(new Task("Уроки"));
+        lessons.setExpanded(true);
+        course.getLessons().forEach(lesson -> {
+            TreeItem lessonItem = new TreeItem(new Task(lesson.getTitle()));
+            lessonItem.setExpanded(true);
+            lesson.getTasks().forEach(task -> {
+                TreeItem taskItem = new TreeItem(new Task(task));
+                lessonItem.getChildren().add(taskItem);
+            });
+            lessons.getChildren().add(lessonItem);
+        });
+        treeTableView.setRoot(lessons);
     }
 }
--- src/main/java/org/verigo/course_project_client/models/Task.java
+++ src/main/java/org/verigo/course_project_client/models/Task.java
@@ -10,12 +10,41 @@ public class Task {
     private String description;
 
     private boolean isHometask;
+    private String isHometaskString;
 
     private int maxPoints;
+    private String maxPointsString;
 
     private Lesson lesson;
 
     private Set<UserTaskResult> usersResults;
+
+
+    public Task() {
+
+    }
+
+    public Task(String title) {
+        this.title = title;
+        this.description = "";
+        this.isHometaskString = "";
+        this.maxPointsString = "";
+    }
+
+    public Task(Task task) {
+        this.title = task.title;
+        this.description = task.description;
+        this.isHometaskString = String.valueOf(task.isHometask);
+        this.maxPointsString = String.valueOf(task.maxPoints);
+    }
+
+    public Task(String title, String description, boolean isHometask, int maxPoints) {
+        this.title = title;
+        this.description = description;
+        this.isHometaskString = String.valueOf(isHometask);
+        this.maxPointsString = String.valueOf(maxPoints);
+    }
+
 
 
     public Integer getId() {
@@ -30,7 +59,7 @@ public class Task {
         return description;
     }
 
-    public boolean isHometask() {
+    public boolean getIsHometask() {
         return isHometask;
     }
 
@@ -58,8 +87,8 @@ public class Task {
         this.description = description;
     }
 
-    public void setHometask(boolean hometask) {
-        isHometask = hometask;
+    public void setIsHometask(boolean isHometask) {
+        this.isHometask = isHometask;
     }
 
     public void setMaxPoints(int maxPoints) {
@@ -72,5 +101,22 @@ public class Task {
 
     public void setUsersResults(Set<UserTaskResult> usersResults) {
         this.usersResults = usersResults;
+    }
+
+
+    public String getIsHometaskString() {
+        return isHometaskString;
+    }
+
+    public String getMaxPointsString() {
+        return maxPointsString;
+    }
+
+    public void setIsHometaskString(String isHometaskString) {
+        this.isHometaskString = isHometaskString;
+    }
+
+    public void setMaxPointsString(String maxPointsString) {
+        this.maxPointsString = maxPointsString;
     }
 }
